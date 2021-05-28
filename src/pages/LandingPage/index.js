c6c28e0d513ab502108a65b51d743ca7f87bbccb
Swanny <swannykathllen@gmail.com>
2021-05-27 21:29:11 -0300

    User now can choose his search

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,36 +1,61 @@
-import React from 'react';
-import BookCard from '../../components/BookCard';
-import api from '../../services/api'
+import React from "react";
+import BookCard from "../../components/BookCard";
+import api from "../../services/api";
 
-import { Container, Column } from './styles';
+import { Container, Column, Header } from "./styles";
 
 const LandingPage = () => {
-
   const [books, setBooks] = React.useState([]);
+  const [search, setSearch] = React.useState("");
 
   const getBooks = () => {
-      const ENDPOINT = `volumes?q=swanny&maxResults=40&key=AIzaSyBtnIxwIJ0tIPwOnh1lzO-FZw54oBB6zRU`;
-      api.get(ENDPOINT).then((response) => {
-      console.log(response.data.items)
-      setBooks(response.data.items);
-    }).catch((error) => {
-      console.log(error)
-    })
+    const ENDPOINT = `volumes?q=${search}&maxResults=40&key=AIzaSyBtnIxwIJ0tIPwOnh1lzO-FZw54oBB6zRU`;
+    api
+      .get(ENDPOINT)
+      .then((response) => {
+        console.log(response.data.items);
+        setBooks(response.data.items);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  React.useEffect(getBooks, []);
+
+  const handleChange = (event) => {
+    //this.setState({value: event.target.value});
+    console.log(event.target.value);
+    setSearch(event.target.value)
   }
 
-  React.useEffect(getBooks,[])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getBooks();
+  }  
 
   return (
     <Container>
+      <Header>
+        <p>Biblioteca Virtual</p>
+        <form onSubmit={handleSubmit}>
+          <input placeholder="O que esta procurando? " type="text" value={search} onChange={handleChange} />
+          <button type="submit">Pesquisar</button>
+        </form>
+      </Header>
       <div className="row">
         {books.map((book) => (
-         <Column className="col-12 col-lg-6 col-xxl-4">
-            <BookCard 
-              bookCover={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://books.google.com/books/content?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"}
+          <Column className="col-12 col-lg-6 col-xxl-4">
+            <BookCard
+              bookCover={
+                book.volumeInfo.imageLinks
+                  ? book.volumeInfo.imageLinks.thumbnail
+                  : "https://books.google.com/books/content?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+              }
               title={book.volumeInfo.title}
               description={book.volumeInfo.description}
-              publishedDate={book.volumeInfo.publishedDate} 
-              infoLink={book.volumeInfo.infoLink} 
+              publishedDate={book.volumeInfo.publishedDate}
+              infoLink={book.volumeInfo.infoLink}
             />
           </Column>
         ))}
--- src/pages/LandingPage/styles.js
+++ src/pages/LandingPage/styles.js
@@ -4,6 +4,33 @@ export const Container = styled.div`
   padding: 0 1vw;
 `;
 
+export const Header = styled.div`
+  display: flex;
+  p{
+    font: 30px Montserrat bold;
+    padding: 2vh 1vw;
+  }
+  form{
+    text-align: right;
+    cursor: pointer;
+    input{
+      font: 20px Montesserat normal;
+      padding: 2vh 1vw;
+      border-radius: 10px;
+    }
+    button{
+    border-radius: 10px;
+    background: #9F352F 0% 0% no-repeat padding-box;
+    color: #ffffff;
+    font: bold 15px Montserrat;
+    cursor: pointer;
+    width: 100px;
+    height: 49px;
+  }
+  }
+   
+`;
+
 export const Column = styled.div`
   display: flex;
   justify-content: center;
