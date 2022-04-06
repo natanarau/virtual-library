e3f1ebf06fbf6cdf866d779593edb19216ce7a19
Swanny Alves <swannykathllen@gmail.com>
2022-04-05 23:03:41 -0300

    style: mudando cor dos botões

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,19 +1,9 @@
-import React from "react";
-import CoverNotFound from "../../assets/imgs/cover-not-found.png";
-import CardActions from "@material-ui/core/CardActions";
-import IconButton from "@material-ui/core/IconButton";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import { useStyles } from "./styles";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import {
-  Card,
-  CardHeader,
-  CardMedia,
-  CardContent,
-  Typography,
-} from "@material-ui/core";
-import { CardContainer } from "./styles";
+import React from "react";
+import CoverNotFound from "../../assets/imgs/coverNotFound.png";
+import * as S from "./styles";
 
 type bookCardProps = {
   bookCover: string;
@@ -34,76 +24,49 @@ export const BookCard: React.FC<bookCardProps> = ({
   handleFavorite,
   isFavorite = false,
 }) => {
-  let classes = useStyles();
   return (
-    <CardContainer>
-      <Card className={classes.card}>
-        <div className={classes.root}>
-          <div className={classes.details}>
-            <CardContent>
-              <CardHeader
-                title={
-                  title && (
-                    <h6>
-                      {`${title.substring(0, 35)} ${
-                        title.length > 35 ? "..." : ""
-                      }`}
-                    </h6>
-                  )
-                }
-              />
-              <CardContent>
-                <Typography
-                  variant="caption"
-                  color="textSecondary"
-                  component="p"
-                >
-                  {"Publicado em: "}
-                  {publishedDate}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {description && (
-                    <p>
-                      Descriçaõ:{" "}
-                      {`${description.substring(0, 160)} ${
-                        description.length > 160 ? "..." : ""
-                      }`}
-                    </p>
-                  )}
-                </Typography>
-              </CardContent>
-            </CardContent>
-          </div>
-          <div>
-            <IconButton
-              className={classes.favorit}
-              aria-label="add to favorites"
-              onClick={handleFavorite}
-            >
-              {isFavorite ? (
-                <FavoriteIcon color="secondary" />
-              ) : (
-                <FavoriteBorderIcon />
-              )}
-            </IconButton>
-            <CardMedia
-              className={classes.cover}
-              image={bookCover ? bookCover : CoverNotFound}
-              title={title}
-            />
-          </div>
-        </div>
-        <CardActions disableSpacing>
-          <IconButton
-            className={classes.expand}
+    <S.BookCardContainer>
+      <S.BookCard>
+        <S.BookCardInfo>
+          <S.BookCardInfoDetails>
+            <S.BookCardTitle>
+              {title &&
+                `${title.substring(0, 35)} ${title.length > 35 ? "..." : ""}`}
+            </S.BookCardTitle>
+            <S.BookCardPublished>
+              Publicado em: {publishedDate}
+            </S.BookCardPublished>
+            <S.BookCardDescription>
+              <span>Descrição:</span>
+              {description &&
+                `${description.substring(0, 160)} ${
+                  description.length > 160 ? "..." : ""
+                }`}
+            </S.BookCardDescription>
+          </S.BookCardInfoDetails>
+          <S.BookCover
+            src={bookCover ? bookCover : CoverNotFound}
+            alt={title}
+          />
+        </S.BookCardInfo>
+        <S.Bottons>
+          <S.BottonSeeMore
             onClick={() => {
               window.open(infoLink, "_blank");
             }}
           >
-            VER MAIS <ArrowForwardIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </CardContainer>
+            Ver Mais <ArrowForwardIcon />
+          </S.BottonSeeMore>
+          <S.BottonFavorite onClick={handleFavorite}>
+            Adicionar aos Favoritos
+            {isFavorite ? (
+              <FavoriteIcon color="secondary" />
+            ) : (
+              <FavoriteBorderIcon />
+            )}
+          </S.BottonFavorite>
+        </S.Bottons>
+      </S.BookCard>
+    </S.BookCardContainer>
   );
 };
--- src/components/BookCard/styles.ts
+++ src/components/BookCard/styles.ts
@@ -1,43 +1,87 @@
 import styled from "styled-components";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
-export const CardContainer = styled.div`
+export const BookCardContainer = styled.div`
   display: flex;
   padding: 2vh 2vw;
   width: 30em;
   flex-direction: column;
 `;
 
-export const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-    },
-    details: {
-      display: "flex",
-      flexDirection: "column",
-    },
-    cover: {
-      width: 133,
-      height: 196,
-      marginRight: 25,
-      marginBottom: 60,
-    },
+export const BookCard = styled.div`
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  border-radius: 20px;
+  padding: 20px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+export const BookCardInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const BookCover = styled.img`
+  width: 133;
+  height: 196;
+`
+export const BookCardInfoDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-right: 20px;
+`
+export const BookCardTitle = styled.div`
+  font-size: 1em;
+  font-weight: bold;
+`
+export const BookCardPublished = styled.div`
+  font-size: 9px;
+  font-weight: lighter;
+  margin-bottom: 20px;
+`
+export const BookCardDescription = styled.div`
+  font-size: 12px;
+  font-weight: lighter;
+ 
+  span{
+    font-weight: bold;
+  }
+`
+export const Bottons = styled.div`
+  display: flex; 
+  justify-content: space-between;
+  margin-top: 20px;
+  @media (max-width: 720px) { 
+    flex-direction: column;
+  }
+`
+export const BottonSeeMore = styled.button`
+  border-style: none;
+  border-radius: 10px;
+  background-color: #0D0D0D;
+  font-size: 13px;
+  height: 2.25rem;  
+  color: #F2F2F2;  
+  width: 100%;
+  margin-right: 1px;
+  @media (max-width: 720px) { 
+   margin-bottom: 1px;
+  }
+`
+export const BottonFavorite = styled.button`
+  border-style: solid ;
+  border-width: 1px;
+  border-color: #0D0D0D;
+  border-radius: 10px;
+  font-size: 13px;
+  height: 2.25rem;
+  background-color: transparent;
+  width: 100%;
+  margin-left: 1px;
+  @media (max-width: 720px) { 
+   margin-top: 1px;
+  }
+`
 
-    expand: {
-      fontSize: "13px",
-      borderRadius: "5px",
-      border: "1px solid",
-      marginBottom: 20,
-      width: "100%",
-    },
-    favorit: {
-      marginLeft: "80px",
-      marginTop: 20,
-    },
-    card: {
-      boxShadow:
-        "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
-    },
-  })
-);
+
+
